c87c51b13f1ca513785d613e63e2d2df210fabda
Osei <albert.osei@takeda.com>
2023-08-07 16:59:59 -0400

    first commit

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv() #getting environment variables from .env
+
+app = Flask(__name__)
+
+@app.route('/<random_string>')
+def return_backwards_string(random_string):
+    return "".join(reversed(random_string))
+
+
+@appg.route('/get-mode')
+def get_mode():
+    return os.environ.get("MODE")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+
+    
